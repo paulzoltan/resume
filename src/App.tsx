e2f68b1ce8edd3c2fe9e872bdf2640df30d48d5f
Paul Zoltán <paulzoltan02@gmail.com>
2023-07-03 11:15:59 +0200

    full screen motion background

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Editor from './components/Editor/Editor'
 import Photo from './components/Photo/Photo'
 import data from './data/resume.json'
 import image from './assets/images/pz.png'
+import MotionBackground from './components/MotionBackground/MotionBackground'
 
 function App() {
   return (
     <>
+      <MotionBackground />
       <Photo image={image} title='pz.png' />
       <Editor data={data} />
     </>
--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,5 +1,4 @@
 import TextArea from '../TextArea/TextArea'
-import MotionBackground from '../MotionBackground/MotionBackground'
 import './editor.css'
 
 export interface EditorProps {
@@ -9,7 +8,6 @@ export interface EditorProps {
 const Editor = ({ data }: EditorProps) => {
   return (
     <div className='editor'>
-      <MotionBackground />
       <div className='editor__title-bar'>
         <div className='editor__icon'>{'{}'} </div>
         resume.json
