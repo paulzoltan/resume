086b07ec7d6e178457fadc304d45cc8daf1d40ed
Paul Zoltán <paulzoltan02@gmail.com>
2023-08-01 08:02:09 +0200

    filtering out undesirable background  videos

--- src/components/MotionBackground/MotionBackground.tsx
+++ src/components/MotionBackground/MotionBackground.tsx
@@ -1,13 +1,9 @@
-import video0 from '../../assets/videos/particles.mp4'
-import video1 from '../../assets/videos/circuit.mp4'
-import video2 from '../../assets/videos/strings.mp4'
-import video3 from '../../assets/videos/cyberpunk.mp4'
-import video4 from '../../assets/videos/giphy.gif'
+import particles from '../../assets/videos/particles.mp4'
+import cyberpunk from '../../assets/videos/cyberpunk.mp4'
 import classNames from 'classnames'
 import './motionBackground.css'
 
-const videos = [video0, video1, video2, video3, video4]
-
+const videos = [particles, cyberpunk]
 const randomNum = Math.floor(Math.random() * videos.length)
 const randomVideo = videos[randomNum]
 /**
@@ -18,7 +14,6 @@ const randomVideoName = randomVideo.split('/').pop()?.split('.')[0]
 
 const MotionBackground = () => {
   return (
-    // <img src={video4} alt='' className={classNames('backgroundVideo')} />
     <video
       className={classNames('backgroundVideo', randomVideoName)}
       autoPlay
@@ -26,7 +21,6 @@ const MotionBackground = () => {
       muted
     >
       <source src={randomVideo} type='video/mp4' />
-      Sorry, your browser doesn't support embedded videos.
     </video>
   )
 }
