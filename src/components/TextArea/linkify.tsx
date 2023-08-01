3fda7a0111e72ce2b9e94b9a91a04e21104312ff
Paul Zoltán <paulzoltan02@gmail.com>
2023-08-01 11:24:52 +0200

    setup a deploy GitHub action

--- src/components/TextArea/linkify.tsx
+++ src/components/TextArea/linkify.tsx
@@ -3,6 +3,6 @@
  */
 export default function linkify(text: string) {
   const url =
-    /^(http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+    /^(http:\/\/|https:\/\/)[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
   return url.test(text) ? <a href={text}>{text}</a> : text
 }
